3df72082300af8bdc635c71bbc07d21cd38d5f90
Adam Nelson <nelsonad@gmail.com>
2024-11-18 22:48:55 -0800

    test

--- src/BuilderPulsePro.Maui/AppShell.xaml.cs
+++ src/BuilderPulsePro.Maui/AppShell.xaml.cs
@@ -13,8 +13,6 @@ public partial class AppShell : Shell, ITransientDependency
 
         Routing.RegisterRoute(nameof(IdentityUserCreateModalPage), typeof(IdentityUserCreateModalPage));
         Routing.RegisterRoute(nameof(IdentityUserEditModalPage), typeof(IdentityUserEditModalPage));
-        Routing.RegisterRoute(nameof(TenantCreatePage), typeof(TenantCreatePage));
-        Routing.RegisterRoute(nameof(TenantEditPage), typeof(TenantEditPage));
         Routing.RegisterRoute(nameof(ProfilePicturePage), typeof(ProfilePicturePage));
         Routing.RegisterRoute(nameof(ChangePasswordPage), typeof(ChangePasswordPage));
         Routing.RegisterRoute(nameof(LoginOrLogoutPage), typeof(LoginOrLogoutPage));
