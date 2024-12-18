53748da50af8d192c24cb4a4ef63f31cdb5ca358
Adam Nelson <nelsonad@gmail.com>
2024-12-18 09:23:39 -0800

    Add dbmigrator connection logging

--- src/BuilderPulsePro.EntityFrameworkCore/EntityFrameworkCore/EntityFrameworkCoreBuilderPulseProDbSchemaMigrator.cs
+++ src/BuilderPulsePro.EntityFrameworkCore/EntityFrameworkCore/EntityFrameworkCoreBuilderPulseProDbSchemaMigrator.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using BuilderPulsePro.Data;
 using Volo.Abp.DependencyInjection;
+using MySqlConnector;
 
 namespace BuilderPulsePro.EntityFrameworkCore;
 
@@ -25,9 +26,31 @@ public class EntityFrameworkCoreBuilderPulseProDbSchemaMigrator
          * current scope.
          */
 
-        await _serviceProvider
-            .GetRequiredService<BuilderPulseProDbContext>()
-            .Database
-            .MigrateAsync();
+        try
+        {
+            await _serviceProvider
+                .GetRequiredService<BuilderPulseProDbContext>()
+                .Database
+                .MigrateAsync();
+        }
+        catch (MySqlException e)
+        {
+            var dbContext = _serviceProvider.GetRequiredService<BuilderPulseProDbContext>();
+            var connectionString = dbContext.Database.GetDbConnection().ConnectionString;
+
+            // Redact sensitive information
+            var safeConnectionString = new MySqlConnectionStringBuilder(connectionString)
+            {
+                Password = "******" // Mask the password
+            }.ToString();
+
+            // Log the detailed error message
+            Console.WriteLine("Unable to connect to the database.");
+            Console.WriteLine($"Connection String: {safeConnectionString}");
+            Console.WriteLine($"Exception: {e.Message}");
+
+            // Optionally rethrow the exception or handle it further
+            throw new Exception($"Database migration failed. Connection String: {safeConnectionString}", e);
+        }
     }
 }
